320c487d08a02748abf32687e6813cc8196f4349
JGM01 <jacobgermanamccray@gmail.com>
2025-12-22 16:33:54 -0500

    .

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -1,4 +1,4 @@
-use crate::{components::Shape, entity::Entity, world::World};
+use crate::{components::Shape, world::World};
 use std::time::Duration;
 
 pub struct PhysicsSystem {
@@ -34,55 +34,5 @@ impl PhysicsSystem {
                 physics.acceleration = [0.0, 0.0];
             }
         }
-    }
-}
-fn resolve_collision(
-    entity_a: &mut Entity,
-    entity_b: &mut Entity,
-    normal: [f32; 2],
-    penetration: f32,
-) {
-    let (phys_a, phys_b) = (
-        entity_a.physics_mut().unwrap(),
-        entity_b.physics_mut().unwrap(),
-    );
-
-    // Separate objects first (position correction)
-    let total_inv_mass = 1.0 / phys_a.mass + 1.0 / phys_b.mass;
-    let correction_a = penetration * (1.0 / phys_a.mass) / total_inv_mass;
-    let correction_b = penetration * (1.0 / phys_b.mass) / total_inv_mass;
-
-    // Move them apart proportional to inverse mass
-    // (heavier objects move less)
-
-    // Velocity correction (the bounce)
-    let relative_velocity = [
-        phys_a.velocity[0] - phys_b.velocity[0],
-        phys_a.velocity[1] - phys_b.velocity[1],
-    ];
-
-    let vel_along_normal = todo!();
-
-    // Don't resolve if velocities are separating
-    if vel_along_normal > 0.0 {
-        return;
-    }
-
-    // Combine restitution (this is where your Option C matters!)
-    let restitution = (phys_a.restitution + phys_b.restitution) / 2.0; // Or geometric mean?
-
-    let impulse_scalar = -(1.0 + restitution) * vel_along_normal / total_inv_mass;
-
-    let impulse = [impulse_scalar * normal[0], impulse_scalar * normal[1]];
-
-    // Apply impulse (force over time) to velocities
-    if phys_a.dynamic {
-        phys_a.velocity[0] += impulse[0] / phys_a.mass;
-        phys_a.velocity[1] += impulse[1] / phys_a.mass;
-    }
-
-    if phys_b.dynamic {
-        phys_b.velocity[0] -= impulse[0] / phys_b.mass;
-        phys_b.velocity[1] -= impulse[1] / phys_b.mass;
     }
 }
